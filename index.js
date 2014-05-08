c3471064b5f2f1368d18b44a837c7184907899bb
James Friend <james.friend@agworld.com.au>
2014-05-08 14:57:38 +0800

    converted to less

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-var through = require( "through" );
-var sass = require( "node-sass" );
-var path = require( "path" );
+var path = require('path');
+var _ = require('underscore');
+var through = require('through');
+var less = require('less');
 
-module.exports = function( file, opts ) {
+module.exports = function(filepath, opts) {
+  config = _.defaults(opts || {}, {
+    compress: false,
+    paths: []
+  });
+
 	var data = "";
-	if( file !== undefined && path.extname( file ) !== ".scss" )
-		return through();
-	else
-		return through( write, end );
+
+	if(file !== undefined && path.extname(file) !== ".less") return through();
+	else return through(write, end);
 
 	function write(buf) {
 		data += buf;
 	}
 
 	function end() {
-		try {
-			this.queue( sass.renderSync( data, opts ) );
-		} catch( err ) {
-			this.emit( 'error', new Error( err ) );
-		}
+		var self = this;
+    var fileConfig = _.clone(config);
 
-		this.queue( null );
+    // Injects the path of the current file.
+    fileConfig.filename = filepath;
+
+    less.render(data, fileConfig, function (err, css) {
+      if (err) {
+        // add a better error message
+        err.message = err.message + ' in file ' + err.filename + ' line no. ' + err.line;
+
+				self.emit('error', new Error(err));
+      } else {
+      	self.queue(css);
+      }
+    	self.queue('null')
+    });
 	}
 };
+
+

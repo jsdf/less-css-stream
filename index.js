0dae6b152e5c69b29f6dea48efecc120863ac8d5
Michael Klein <michaelklein27@gmail.com>
2015-04-27 11:03:59 +0200

    fixed formatting, removed dependency on underscore

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-var path = require('path');
-var _ = require('underscore');
 var through = require('through');
 var less = require('less');
+var path = require('path');
+var extend = require('extend-object');
 
-module.exports = function(filepath, opts) {
-  config = _.defaults(opts || {}, {
-    compress: false,
-    paths: []
-  });
+module.exports = function(file, opts) {
+    config = extend({
+      compress: false,
+      paths: []
+    }, opts);
 
-	var data = "";
+    var data = '';
+    if (file !== undefined && path.extname(file) !== '.less') {
+        return through();
+    } else {
+        return through(write, end);
+    }
 
-	if(filepath !== undefined && path.extname(filepath) !== ".less") return through();
-	else return through(write, end);
+    function write(buf) {
+        data += buf;
+    }
 
-	function write(buf) {
-		data += buf;
-	}
+    function end() {
+        var self = this;
+        var fileConfig = extend({}, config);
 
-	function end() {
-		var self = this;
-    var fileConfig = _.clone(config);
+        // Injects the path of the current file.
+        fileConfig.filename = file;
 
-    // Injects the path of the current file.
-    fileConfig.filename = filepath;
-
-    less.render(data, fileConfig, function (err, css) {
-      if (err) {
-        // add a better error message
-        err.message = err.message + ' in file ' + err.filename + ' line no. ' + err.line;
-
-				self.emit('error', new Error(err));
-      } else {
-      	self.queue(css);
-      }
-    	self.queue(null)
-    });
-	}
+        less.render(data, fileConfig, function (err, css) {
+            if (err) {
+                // TODO: add a better error message
+                err.message = err.message + ' in file ' + err.filename + ' line no. ' + err.line;
+                self.emit('error', new Error(err));
+            } else {
+                self.queue(css);
+            }
+            self.queue(null)
+        });
+    }
 };
 

a5ebbe3a1f714309a71cc93105c78bf7eb4f8dcc
James Friend <james@jsdf.co>
2015-04-28 21:54:17 +1000

    added test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 var lessCssStream = require('../');
 var fs = require('fs');
 var path = require('path');
+var concatStream = require('concat-stream');
+var test = require("tap").test;
 
-var inputFile = path.join(__dirname, 'sampleStyle.less');
+test("it compiles less", function (t) {
+  t.plan(1);
+  var inputFile = path.join(__dirname, 'sampleStyle.less');
+  var expectedFile = path.join(__dirname, 'expected.css');
 
-fs.createReadStream(inputFile).pipe(lessCssStream(inputFile, { compress: true })).pipe(process.stdout);
+  fs.createReadStream(inputFile)
+    .pipe(lessCssStream(inputFile, {compress: true}))
+    .pipe(concatStream(function(output) {
+      var expected = fs.readFileSync(expectedFile, {encoding: 'utf8'});
+      t.equal(output, expected, "actual output should be same as expected");
+      t.end();
+    }));
+});

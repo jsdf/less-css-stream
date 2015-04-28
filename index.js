5d02d36f2910426091fe87f5b8d64832ce175575
James Friend <james@jsdf.co>
2015-04-28 21:52:48 +1000

    code style normalisation

--- index.js
+++ index.js
@@ -4,51 +4,51 @@ var path = require('path');
 var extend = require('extend-object');
 
 module.exports = function(file, opts) {
-    var data = '',
-    config = extend({
-      compress: false,
-      paths: []
-    }, opts);
+  var data = '',
+  config = extend({
+    compress: false,
+    paths: []
+  }, opts);
 
-    if (!/\.css$|\.less$/.test(file)) {
-        return through();
+  if (!/\.css$|\.less$/.test(file)) {
+    return through();
+  }
+
+  return through(write, end);
+
+  function write(buf) {
+    data += buf;
+  }
+
+  function end() {
+    var self = this;
+    var fileConfig = extend({}, config);
+
+    // Injects the path of the current file.
+    fileConfig.filename = file;
+
+    less.render(data, fileConfig, function (err, output) {
+      if (err) {
+        self.emit('error', new Error(getErrorMessage(err), file, err.line));
+      } else {
+        self.queue(output.css);
+      }
+      self.queue(null);
+    });
+  }
+
+  function getErrorMessage(err) {
+    var msg = err.message;
+    if (err.line) {
+      msg += ", line " + err.line;
+    }
+    if (err.column) {
+      msg += ", column " + err.column;
+    }
+    if (err.extract) {
+      msg += ": \"" + err.extract + "\"";
     }
 
-    return through(write, end);
-
-    function write(buf) {
-        data += buf;
-    }
-
-    function end() {
-        var self = this;
-        var fileConfig = extend({}, config);
-
-        // Injects the path of the current file.
-        fileConfig.filename = file;
-
-        less.render(data, fileConfig, function (err, output) {
-            if (err) {
-                self.emit('error', new Error(getErrorMessage(err), file, err.line));
-            } else {
-                self.queue(output.css);
-            }
-            self.queue(null);
-        });
-    }
-
-    function getErrorMessage(err) {
-        var msg = err.message;
-		if (err.line) {
-			msg += ", line " + err.line;
-		}
-		if (err.column) {
-			msg += ", column " + err.column;
-		}
-		if (err.extract) {
-			msg += ": \"" + err.extract + "\"";
-		}
-
-		return msg;
-    }
+    return msg;
+  }
 };
